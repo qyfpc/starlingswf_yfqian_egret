15a64ae413ae412c6894816f3ce55ef610483097
qyfpc <qyfpc@163.com>
2015-08-25 15:33:13 +0800

    框架结构更新
    增加使用说明和框架结构说明

---
+++ launcher/egret_loader.js
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2014,Egret-Labs.org
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Egret-Labs.org nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY EGRET-LABS.ORG AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL EGRET-LABS.ORG AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+egret_h5.startGame = function () {
+//    egret.RendererContext.imageSmoothingEnabled = false;
+    var  context = egret.MainContext.instance;
+    context.touchContext = new egret.HTML5TouchContext();
+    context.deviceContext = new egret.HTML5DeviceContext();
+    context.netContext = new egret.HTML5NetContext();
+
+    egret.StageDelegate.getInstance().setDesignSize(480, 800);
+    context.stage = new egret.Stage();
+
+    var scaleMode;
+    var bs={
+        versions:function(){
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {//移动终端浏览器版本信息
+                trident: u.indexOf('Trident') > -1, //IE内核
+                presto: u.indexOf('Presto') > -1, //opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+                iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, //是否iPad
+                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+        }(),
+        language:(navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+    if(bs.versions.mobile){
+        if(bs.versions.android||bs.versions.iPhone||bs.versions.ios){
+            scaleMode=egret.StageScaleMode.NO_BORDER;
+        }else if(bs.versions.iPad){
+            scaleMode =  egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
+        }
+        else
+        {
+            scaleMode =  egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
+        }
+    }
+//    var scaleMode =  egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
+     var scaleModeAll=egret.StageScaleMode.NO_BORDER;
+    context.stage.scaleMode = scaleMode;
+
+    //WebGL is a Egret's beta property. It's off by default.
+    //WebGL是egret的Beta特性，默认关闭
+    var rendererType = 0;
+    if (rendererType == 1) {// egret.WebGLUtils.checkCanUseWebGL()) {
+        console.log("Use WebGL mode");
+        context.rendererContext = new egret.WebGLRenderer();
+    }
+    else {
+        context.rendererContext = new egret.HTML5CanvasRenderer();
+    }
+
+    egret.MainContext.instance.rendererContext.texture_scale_factor = 1;
+    context.run();
+
+    var rootClass;
+    if(document_class){
+        rootClass = egret.getDefinitionByName(document_class);
+    }
+    if(rootClass) {
+        var rootContainer = new rootClass();
+        if(rootContainer instanceof egret.DisplayObjectContainer){
+            context.stage.addChild(rootContainer);
+        }
+        else{
+            throw new Error("Document Class must be the subclass to egret.DisplayObjectContainer!");
+        }
+    }
+    else{
+        throw new Error("Document Class is not found！");
+    }
+
+    //处理屏幕大小改变
+    //implement for screen size change
+    var resizeTimer = null;
+    var doResize = function () {
+        context.stage.changeSize();
+        resizeTimer = null;
+    };
+    window.onresize = function () {
+        if (resizeTimer == null) {
+            resizeTimer = setTimeout(doResize, 300);
+        }
+    };
+};
